0a0df890ffc4f83712a558a3ebe82bcdc6db1275
Nimish Dudhe <20cs3043@rgipt.ac.in>
2022-10-20 18:43:35 +0530

    Add support for printing different types of instructions.
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- dic.sh
+++ dic.sh
@@ -10,3 +10,24 @@ end" > ~/.gdbinit
 
 echo Total Number Of Instructions \: $(cat $1_ins | wc -l)
 echo ""
+echo Number of Instructions of Different Types \:
+
+i=0
+
+cat $1_ins | cut -d ":" -f 2 | awk '{$1=$1};1' | cut -d " " -f 1 | sort | uniq | while read operand
+do
+	echo -n '\t'$operand : $(cat $1_ins | cut -d ":" -f 2 | awk '{$1=$1};1' | cut -d " " -f 1 | grep $operand | wc -l)'\t\t'
+	i=$((($i)+1))
+	if [ $((($i)%3)) -eq 0 ]
+	then
+		echo '\n'
+	fi
+done
+
+echo ""
+if [ $((($(cat $1_ins | cut -d ":" -f 2 | awk '{$1=$1};1' | cut -d " " -f 1 | sort | uniq | wc -l))%3)) -eq 0 ]
+then
+	echo "">/dev/null
+else
+	echo ""
+fi
