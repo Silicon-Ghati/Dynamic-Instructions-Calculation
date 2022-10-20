ab071eec9b6e689328bd2afbffe89394c9344510
Nimish Dudhe <20cs3043@rgipt.ac.in>
2022-10-20 18:47:08 +0530

    remove gdb_instructions file and add support for arguments.
    This will pass the required commands from the orignal script itself.
    
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- dic.sh
+++ dic.sh
@@ -6,6 +6,19 @@ define hook-stop
 x/1i \$rip
 end" > ~/.gdbinit
 
+if [ $# -eq 2 ]
+then
+	echo "IyEvdXNyL2Jpbi9weXRob24zCgpwcmludCgic3RhcnRpIDwgaW5wdXRfZmlsZSIpCgpmb3IgaSBpbiByYW5nZSAoMTUwMDAwKToKCXByaW50KCJzaSIpCg==" | base64 -d > gdb_instructions
+	echo $2 | xargs -I{} sh -c "sed -i 's/input_file/{}/g' gdb_instructions"
+elif [ $# -eq 1 ]
+then
+	echo "IyEvdXNyL2Jpbi9weXRob24zCgpwcmludCgic3RhcnRpIikKCmZvciBpIGluIHJhbmdlICgxNTAwMDApOgoJcHJpbnQoInNpIikK" | base64 -d > gdb_instructions
+else
+	echo "Usage : ./dic.sh binary input_file(optional)\nUse the input file if the program needs user input"
+	exit
+fi
+chmod +x gdb_instructions
+
 ./gdb_instructions | gdb $1 2>/dev/null | grep "=>" | cut -c 10- > $1_ins
 
 echo Total Number Of Instructions \: $(cat $1_ins | wc -l)
