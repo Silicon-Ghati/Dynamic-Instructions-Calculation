8d20832e839d8d69e2de3e1f78874affc3862984
AyushR1 <ayushrathore12501@gmail.com>
2022-12-05 22:38:27 +0530

    Build an REST API providing analysis

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import shlex
 
 app = Flask(__name__)
 
+host ="https://instructions.pythonanywhere.com/"
 app.config["UPLOAD_FOLDER"] = "/home/instructions/dic/static/dic_temp_files/"
 
 @app.route('/')
@@ -26,6 +27,25 @@ def save_file():
         subprocess.call(shlex.split('bash /home/instructions/dic/dic.sh ' + app.config['UPLOAD_FOLDER'] + str(filename)))
     
     return render_template('contenthome.html')
+
+@app.route('/instructions_api', methods=['POST'])
+def ins_api():
+    if request.method == 'POST':
+        f = request.files['file']
+        global filename
+        filename = secure_filename(f.filename)
+
+        f.save(app.config['UPLOAD_FOLDER'] + filename)
+        subprocess.call(shlex.split('./dic.sh ' + app.config['UPLOAD_FOLDER'] + str(filename)))
+        a = host + app.config['UPLOAD_FOLDER'] + filename + '_analysis'
+        i = host + app.config['UPLOAD_FOLDER'] + filename + '_ins'
+        c = host +app.config['UPLOAD_FOLDER'] + filename + '_chart'
+
+    return {"analysis": a,
+            "Instruction": i,
+            "Chart": c
+    }
+
 
 @app.route('/analysis')
 def analysis():
